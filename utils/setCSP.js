1197adddbac278bc1c9b68f4a0ac679235addb2e
RafaelVoss <rafaelvoss94@gmail.com>
2024-01-31 11:57:37 -0300

    updated setCSP.js in utils

--- utils/setCSP.js
+++ utils/setCSP.js
@@ -10,6 +10,7 @@ exports.setCSP = (req, res, next) => {
           'https://*.mapbox.com',
           'https://fonts.googleapis.com/',
           'https://js.stripe.com/v3/m-outer-27c67c0d52761104439bb051c7856ab1.html#url=http%3A%2F%2F127.0.0.1%3A3000%2Ftour%2Fthe-park-camper&title=Natours%20%7C%20The%20Park%20Camper%20Tour&referrer=&muid=NA&sid=NA&version=6&preview=false',
+          'https://js.stripe.com/v3/m-outer-3437aaddcdf6922d623e172c2d6f9278.html#url=https%3A%2F%2Fnatours-project-0iop.onrender.com%2Ftour%2Fthe-sea-explorer&title=Natours%20%7C%20The%20Sea%20Explorer%20Tour&referrer=&muid=NA&sid=NA&version=6&preview=false',
         ],
         'connect-src': [
           "'self'",
@@ -20,7 +21,8 @@ exports.setCSP = (req, res, next) => {
           'https://events.mapbox.com/',
           'http://127.0.0.1:3000/api/v1/users/login',
           'http://127.0.0.1:3000/api/v1/users/updateMe',
-          ' https://js.stripe.com/v3/.deploy_status_henson.json',
+          'https://js.stripe.com/v3/.deploy_status_henson.json',
+          'wss://natours-project-0iop.onrender.com:1234/',
         ],
         'base-uri': ["'self'"],
         'font-src': ["'self'", 'https:', 'data:'],
