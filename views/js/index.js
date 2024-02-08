a09ccb75d199534b8796dd66c476924acb84923a
Rafael <rafaelvoss94@gmail.com>
2024-02-08 16:24:28 -0300

    added alert functionality on the query string

--- views/js/index.js
+++ views/js/index.js
@@ -5,6 +5,7 @@
 
 //import '@babel/polyfill'; =====> test with try catch block!!!!!!!
 import { displayMap } from './mapbox.js';
+import { showAlert } from './alerts.js';
 import { updateData } from './updateSettings.js';
 
 // DOM ELEMENTS
@@ -15,3 +16,6 @@ if (mapBox) {
     const locations = JSON.parse(mapBox.dataset.locations);
     displayMap(locations);
 }
+
+const alertMessage = document.querySelector('body').dataset.alert;
+if (alert) showAllert('success', alertMessage, 20);
